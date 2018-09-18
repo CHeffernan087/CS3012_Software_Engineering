4857ef81a72c2a487f9fee634e495a6948122823
CHeffernan087 <CHeffernan087@gmail.com>
2018-09-18 20:39:48 +0100

    BST class fleshed out.Most og generic functionality implemented

--- BST.py
+++ BST.py
@@ -1,15 +1,59 @@
 from graph_lowestCommonAncestor import Node
 class BST:
+	root = None
+	
 
-	def put(key, value):
+	def put_recursive(self,n, key, val):
+
+		if(n.getKey() > key):
+			if(n.left==None):
+				n.left = Node(key,val)
+			else:
+				self.put_recursive(n.left,key,val)
+		elif(n.getKey() < key):
+			if(n.right==None):
+				n.right = Node(key,val)
+			else:
+				self.put_recursive(n.right,key,val)
+		else:
+			n.val = val
+
+
+	def put(self,key, value):
 		if(self.root== None):
 			self.root = Node(key,value)
+			self.left=None
+			self.right = None
 
 		else:
-			put(self.root,key,value)
+			self.put_recursive(self.root,key,value)
 
-	def put(n, key, val):
-		print("test")
+
+
+	def printInOrder(self):
+		if(self.root!=None):
+			self.printInOrderRecursive(self.root)
+			
+	def printInOrderRecursive(self,n):
+
+		if(n==None):
+			return
+		self.printInOrderRecursive(n.left)
+		n.printNode()
+		self.printInOrderRecursive(n.right)
+
+
+	def findLeftMostBranch(self,n):
+		if(n.left == None):
+			return n
+		findLeftMostBranch(n.left)
+
+	
+
 
 my_list = BST();
 my_list.put(9,"Conor") 
+my_list.put(12,"Kate") 
+my_list.put(10,"James") 
+my_list.put(7,"Fearghal") 
+my_list.printInOrder()
--- graph_lowestCommonAncestor.py
+++ graph_lowestCommonAncestor.py
@@ -1,13 +1,22 @@
 class Node:
-  def __init__(self, key, value):
-	self.key = key
-	self.value = value
-	
-  def printNode(self):
-	print(self.key+ " : "+ str(self.value))
+	left = None
+	right = None
+  	def __init__(self, key, value):
+		self.key = key
+		self.value = value
+	def printNode(self):
+		print(str(self.key)+" : "+self.value)
+
+	def getKey(self):
+		return self.key
+
+
+	def toString(self):
+		return str(self.key)+" : "+self.value
 		
 
+
 node = Node("Conor",1)
-node.printNode()
+#node.printNode()
 
 
