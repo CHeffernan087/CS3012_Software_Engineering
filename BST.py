0bb47d4c2e7d423bc4c2d2bbf5caf309d8d6e48b
CHeffernan087 <CHeffernan087@gmail.com>
2018-09-18 17:15:30 +0100

    basis of BST class made. First successful python import

---
+++ BST.py
@@ -0,0 +1,15 @@
+from graph_lowestCommonAncestor import Node
+class BST:
+
+	def put(key, value):
+		if(self.root== None):
+			self.root = Node(key,value)
+
+		else:
+			put(self.root,key,value)
+
+	def put(n, key, val):
+		print("test")
+
+my_list = BST();
+my_list.put(9,"Conor") 
