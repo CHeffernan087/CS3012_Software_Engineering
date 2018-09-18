2570fc078d013fe02cad15d0638deb0d148060d7
CHeffernan087 <CHeffernan087@gmail.com>
2018-09-18 16:48:25 +0100

    first working object oriented code in python

--- graph_lowestCommonAncestor.py
+++ graph_lowestCommonAncestor.py
@@ -1,1 +1,13 @@
-print("Hello_World");
+class Node:
+  def __init__(self, key, value):
+	self.key = key
+	self.value = value
+	
+  def printNode(self):
+	print(self.key+ " : "+ str(self.value))
+		
+
+node = Node("Conor",1)
+node.printNode()
+
+
